3d2377223ca91691bc23bfaacb75215bc6b8cba4
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-26 17:33:14 -0300

    Add MyApp types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
+import { AppProps } from 'next/app'
 import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
