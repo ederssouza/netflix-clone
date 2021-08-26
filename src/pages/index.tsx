837694c22f54168e085f4b0b23ce8c2e4aa35065
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-26 18:27:35 -0300

    Change page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import Head from 'next/head'
+
 import { FeaturedMovie } from '../components/FeaturedMovie'
 import { Header } from '../components/Header'
 import { MoviesCarousel } from '../components/MoviesCarousel'
 import { Footer } from '../components/Footer'
+
 import styles from './home.module.scss'
 
 export default function Home () {
   return (
     <>
       <Head>
-        <title>Netflix clone</title>
+        <title>Início | Netflix</title>
         <meta name="description" content="..." />
         <link rel="icon" href="/assets/img/favicon.ico" />
       </Head>
