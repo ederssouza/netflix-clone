db4db8a37d22b251dc5a6c49963975652878c0a7
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-26 17:10:50 -0300

    Add basic project structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Netflix clone</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/assets/img/favicon.ico" />
+      </Head>
+
+      <main className={styles.container}>
+        <h1 className={styles.title}>
+          <Image
+            src="/assets/img/netflix.png"
+            width="92.5px"
+            height="31px"
+            alt="Netflix logo"
+          />
+          Netflix clone
+        </h1>
+      </main>
+    </>
+  )
+}
