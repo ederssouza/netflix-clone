ed74423ca9abeb6f1e91ba207bb827864370abfb
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-26 17:33:01 -0300

    Add eslint JavaScript Standard Style config

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,7 +2,5 @@
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
-declare module '*.scss'
-
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
