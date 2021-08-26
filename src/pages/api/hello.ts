a88325641d7c7347a65b17d999d800e89495a3ff
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-26 17:33:41 -0300

    Add api types

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js API route support: https://nextjs.org/docs/basic-features/typescript#api-routes
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req, res) {
+export default function hello (req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({ name: 'John Doe' })
 }
