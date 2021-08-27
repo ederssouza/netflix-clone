21eb017991c821c7804715af555a879075e280c1
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-27 14:28:05 -0300

    Add MoviesCarousel component style

--- src/components/MoviesCarousel/MoviesCarousel.spec.tsx
+++ src/components/MoviesCarousel/MoviesCarousel.spec.tsx
@@ -1,9 +1,23 @@
 import { render, screen } from '@testing-library/react'
 import { MoviesCarousel } from '.'
 
+const movies = Array.from({ length: 20 }).map((_, index) => {
+  return {
+    id: index + 1,
+    title: 'Movie title',
+    image: 'https://occ-0-626-1123.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQ1n7nMsHQU4eAT8s7OtRNBTkRt8KcVP9M0q5ZnZRppX0WwulOFBD6wHx0U4pOga86psNHvwzBvJah7ey3BpIgbMOeE.webp?r=252'
+  }
+})
+
 describe('MoviesCarousel component', () => {
   it('should render with success', () => {
-    render(<MoviesCarousel />)
-    expect(screen.getByText('MoviesCarousel component')).toBeInTheDocument()
+    render(<MoviesCarousel title="Adventure" movies={movies} />)
+    expect(screen.getByText('Adventure')).toBeInTheDocument()
+  })
+
+  it('should render a list movie', () => {
+    render(<MoviesCarousel title="Adventure" movies={movies} />)
+    const moviesImageList = screen.getAllByTestId('movie-img')
+    expect(moviesImageList.length).toBe(20)
   })
 })
