d02729ed4ce0c79933da00006bf80be89cd8c252
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-27 18:04:06 -0300

    Add MoviesCarousel tests

--- src/components/MoviesCarousel/MoviesCarousel.spec.tsx
+++ src/components/MoviesCarousel/MoviesCarousel.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { MoviesCarousel } from '.'
 
 const movies = Array.from({ length: 20 }).map((_, index) => {
@@ -9,15 +9,65 @@ const movies = Array.from({ length: 20 }).map((_, index) => {
   }
 })
 
+const originalOffsetHeight = Object.getOwnPropertyDescriptor(
+  HTMLElement.prototype,
+  'offsetHeight'
+)
+
+const originalOffsetWidth = Object.getOwnPropertyDescriptor(
+  HTMLElement.prototype,
+  'offsetWidth'
+)
+
 describe('MoviesCarousel component', () => {
+  beforeAll(() => {
+    Object.defineProperty(HTMLElement.prototype, 'offsetHeight', {
+      configurable: true,
+      value: 200
+    })
+
+    Object.defineProperty(HTMLElement.prototype, 'offsetWidth', {
+      configurable: true,
+      value: 200
+    })
+  })
+
   it('should render with success', () => {
     render(<MoviesCarousel title="Adventure" movies={movies} />)
     expect(screen.getByText('Adventure')).toBeInTheDocument()
   })
 
-  it('should render a list movie', () => {
+  it('should render a movie list', () => {
     render(<MoviesCarousel title="Adventure" movies={movies} />)
-    const moviesImageList = screen.getAllByTestId('movie-img')
-    expect(moviesImageList.length).toBe(20)
+    const $moviesImageList = screen.getAllByTestId('movie-img')
+    expect($moviesImageList.length).toBe(20)
+  })
+
+  it('should move the scroll movie list to X, when click next button', () => {
+    render(<MoviesCarousel title="Adventure" movies={movies} />)
+
+    const $containerList = screen.getByTestId('movie-list')
+    const $buttonNextMovies = screen.getByTestId('button-next-movies')
+
+    fireEvent.click($buttonNextMovies)
+    expect($containerList.scrollLeft).toEqual(200)
+  })
+
+  afterAll(() => {
+    Object.defineProperty(HTMLElement.prototype, 'offsetHeight', originalOffsetHeight)
+    Object.defineProperty(HTMLElement.prototype, 'offsetWidth', originalOffsetWidth)
+  })
+
+  it('should move the scroll movie list to X, when click prev button', () => {
+    render(<MoviesCarousel title="Adventure" movies={movies} />)
+
+    const $containerList = screen.getByTestId('movie-list')
+    const $buttonNextMovies = screen.getByTestId('button-next-movies')
+    const $buttonPrevMovies = screen.getByTestId('button-prev-movies')
+
+    fireEvent.click($buttonNextMovies)
+    fireEvent.click($buttonPrevMovies)
+
+    expect($containerList.scrollLeft).toEqual(0)
   })
 })
