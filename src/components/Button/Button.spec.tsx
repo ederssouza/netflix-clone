1b4439f068156d97a667dd9fd38db0dcce187737
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-27 12:30:27 -0300

    Add more tests

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,9 +1,41 @@
 import { render, screen } from '@testing-library/react'
+import { IoMdPlay } from 'react-icons/io'
 import { Button } from '.'
 
 describe('Button component', () => {
   it('should render with success', () => {
-    render(<Button>Primary</Button>)
-    expect(screen.getByText('Primary')).toBeInTheDocument()
+    render(<Button>Primary button</Button>)
+
+    const component = screen.getByText('Primary button')
+    expect(component).toBeInTheDocument()
+    expect(component).toHaveClass('btnPrimary')
+  })
+
+  it('should render `btnPrimary` CSS class when not receive `color` prop', () => {
+    render(<Button>Primary button</Button>)
+
+    const component = screen.getByText('Primary button')
+    expect(component).toHaveClass('btnPrimary')
+  })
+
+  it('should render `btnSecondary` CSS class when receive `color="secondary"` prop', () => {
+    render(
+      <Button color="secondary">
+        Secondary button
+      </Button>
+    )
+
+    const component = screen.getByText('Secondary button')
+    expect(component).toHaveClass('btnSecondary')
+  })
+
+  it('should render button with an icon when receiving `icon` prop', () => {
+    const { container } = render(
+      <Button icon={<IoMdPlay />}>
+        Play movie
+      </Button>
+    )
+
+    expect(container.querySelector('svg')).toBeInTheDocument()
   })
 })
