ca3273390b0a16d747038dc3c35e907bf42b7e0d
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-27 11:29:52 -0300

    Add Jest config

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  setupFilesAfterEnv: [
+    '<rootDir>/src/tests/setupTests.ts'
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  },
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(scss|css|sass)$': 'identity-obj-proxy'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.tsx',
+    '!src/**/*.spec.tsx',
+    '!src/pages/_app.tsx',
+    '!src/pages/_document.tsx'
+  ],
+  coverageReporters: ['json', 'lcov']
+}
