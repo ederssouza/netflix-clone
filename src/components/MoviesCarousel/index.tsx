d1ef8e7769ff179e3a6c816d0f44d35978e1507f
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-27 18:03:59 -0300

    Implement MoviesCarousel actions

--- src/components/MoviesCarousel/index.tsx
+++ src/components/MoviesCarousel/index.tsx
@@ -1,5 +1,91 @@
-export function MoviesCarousel () {
+import { useState, useEffect, useRef } from 'react'
+import Image from 'next/image'
+import styles from './styles.module.scss'
+
+interface IMoviesCarousel {
+  title: string
+  movies: any[]
+}
+
+interface IHandleScrollParams {
+  orientation: 'left' | 'right'
+}
+
+export function MoviesCarousel ({ title, movies }: IMoviesCarousel) {
+  const refContainer = useRef(null)
+  const [movieListWidth, setMovieListWidth] = useState(0)
+  const [imageWidth, setImageWidth] = useState(0)
+
+  function getMaxWidthScrollByClick () {
+    return movieListWidth && imageWidth
+      ? Math.floor(movieListWidth / imageWidth)
+      : 0
+  }
+
+  function handleScroll ({ orientation }: IHandleScrollParams): void {
+    const $movieList = refContainer?.current?.querySelector(`.${styles.list}`)
+
+    if ($movieList && orientation === 'left') {
+      $movieList.scrollLeft -= getMaxWidthScrollByClick() * imageWidth
+    }
+
+    if ($movieList && orientation === 'right') {
+      $movieList.scrollLeft += getMaxWidthScrollByClick() * imageWidth
+    }
+  }
+
+  useEffect(() => {
+    const container = refContainer.current
+    const movieList = container.querySelector(`.${styles.list}`)
+    const imageWidth = container.querySelector(`.${styles.listItem}`).offsetWidth
+
+    setMovieListWidth(movieList.offsetWidth)
+    setImageWidth(imageWidth)
+  }, [])
+
   return (
-    <div>MoviesCarousel component</div>
+    <div className={styles.container}>
+      <h2 className={styles.title}>{title}</h2>
+
+      <div
+        className={styles.listContainer}
+        ref={refContainer}
+      >
+        <button
+          className={styles.prevButton}
+          data-testid="button-prev-movies"
+          onClick={() => handleScroll({ orientation: 'left' })}
+        >
+          Anterior
+        </button>
+
+        <div
+          className={styles.list}
+          data-testid="movie-list"
+        >
+          {movies.map(movie => (
+            <div key={movie.id} className={styles.listItem}>
+              <a href="#" title={movie.title}>
+                <Image
+                  src={movie.image}
+                  alt={movie.title}
+                  width="251"
+                  height="141.31"
+                  data-testid="movie-img"
+                />
+              </a>
+            </div>
+          ))}
+        </div>
+
+        <button
+          className={styles.nextButton}
+          data-testid="button-next-movies"
+          onClick={() => handleScroll({ orientation: 'right' })}
+        >
+          Próximo
+        </button>
+      </div>
+    </div>
   )
 }
