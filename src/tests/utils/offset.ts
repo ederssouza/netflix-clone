ef61796514af17ccd16fd801c1ff7c5943577f9a
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-27 18:10:36 -0300

    Add offset utils

---
+++ src/tests/utils/offset.ts
@@ -0,0 +1,26 @@
+const originalOffsetHeight = Object.getOwnPropertyDescriptor(
+  HTMLElement.prototype,
+  'offsetHeight'
+)
+
+const originalOffsetWidth = Object.getOwnPropertyDescriptor(
+  HTMLElement.prototype,
+  'offsetWidth'
+)
+
+export function offsetBeforeAll () {
+  Object.defineProperty(HTMLElement.prototype, 'offsetHeight', {
+    configurable: true,
+    value: 200
+  })
+
+  Object.defineProperty(HTMLElement.prototype, 'offsetWidth', {
+    configurable: true,
+    value: 200
+  })
+}
+
+export function offsetAfterAll () {
+  Object.defineProperty(HTMLElement.prototype, 'offsetHeight', originalOffsetHeight)
+  Object.defineProperty(HTMLElement.prototype, 'offsetWidth', originalOffsetWidth)
+}
