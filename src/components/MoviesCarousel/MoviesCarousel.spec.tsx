b50e929cb71b1a99190c3f645262dc01b6f77eb9
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-27 18:10:55 -0300

    Remove dead code

--- src/components/MoviesCarousel/MoviesCarousel.spec.tsx
+++ src/components/MoviesCarousel/MoviesCarousel.spec.tsx
@@ -1,36 +1,10 @@
 import { render, screen, fireEvent } from '@testing-library/react'
 import { MoviesCarousel } from '.'
-
-const movies = Array.from({ length: 20 }).map((_, index) => {
-  return {
-    id: index + 1,
-    title: 'Movie title',
-    image: 'https://occ-0-626-1123.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQ1n7nMsHQU4eAT8s7OtRNBTkRt8KcVP9M0q5ZnZRppX0WwulOFBD6wHx0U4pOga86psNHvwzBvJah7ey3BpIgbMOeE.webp?r=252'
-  }
-})
-
-const originalOffsetHeight = Object.getOwnPropertyDescriptor(
-  HTMLElement.prototype,
-  'offsetHeight'
-)
-
-const originalOffsetWidth = Object.getOwnPropertyDescriptor(
-  HTMLElement.prototype,
-  'offsetWidth'
-)
+import { movies } from '../../tests/mocks/movies'
+import { offsetAfterAll, offsetBeforeAll } from '../../tests/utils/offset'
 
 describe('MoviesCarousel component', () => {
-  beforeAll(() => {
-    Object.defineProperty(HTMLElement.prototype, 'offsetHeight', {
-      configurable: true,
-      value: 200
-    })
-
-    Object.defineProperty(HTMLElement.prototype, 'offsetWidth', {
-      configurable: true,
-      value: 200
-    })
-  })
+  beforeAll(() => offsetBeforeAll())
 
   it('should render with success', () => {
     render(<MoviesCarousel title="Adventure" movies={movies} />)
@@ -50,12 +24,8 @@ describe('MoviesCarousel component', () => {
     const $buttonNextMovies = screen.getByTestId('button-next-movies')
 
     fireEvent.click($buttonNextMovies)
-    expect($containerList.scrollLeft).toEqual(200)
-  })
 
-  afterAll(() => {
-    Object.defineProperty(HTMLElement.prototype, 'offsetHeight', originalOffsetHeight)
-    Object.defineProperty(HTMLElement.prototype, 'offsetWidth', originalOffsetWidth)
+    expect($containerList.scrollLeft).toEqual(200)
   })
 
   it('should move the scroll movie list to X, when click prev button', () => {
@@ -70,4 +40,6 @@ describe('MoviesCarousel component', () => {
 
     expect($containerList.scrollLeft).toEqual(0)
   })
+
+  afterAll(() => offsetAfterAll())
 })
