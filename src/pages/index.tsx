30dd4ab5e9937707a6bbb3c573debdea865f8f68
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-27 14:28:17 -0300

    Add movies mock

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,14 @@ import { Footer } from '../components/Footer'
 import styles from './home.module.scss'
 
 export default function Home () {
+  const movies = Array.from({ length: 20 }).map((_, index) => {
+    return {
+      id: index + 1,
+      title: 'Movie title',
+      image: 'https://occ-0-626-1123.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQ1n7nMsHQU4eAT8s7OtRNBTkRt8KcVP9M0q5ZnZRppX0WwulOFBD6wHx0U4pOga86psNHvwzBvJah7ey3BpIgbMOeE.webp?r=252'
+    }
+  })
+
   return (
     <>
       <Head>
@@ -19,7 +27,7 @@ export default function Home () {
       <main className={styles.container}>
         <Header />
         <FeaturedMovie />
-        <MoviesCarousel />
+        <MoviesCarousel title="Adventure" movies={movies} />
         <Footer />
       </main>
     </>
