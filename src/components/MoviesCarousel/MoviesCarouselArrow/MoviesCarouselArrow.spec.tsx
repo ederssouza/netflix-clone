807a5f7eda4bb4c2b469f36b020beb33d507ab4f
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-28 10:38:22 -0300

    Create MoviesCarouselArrow test

---
+++ src/components/MoviesCarousel/MoviesCarouselArrow/MoviesCarouselArrow.spec.tsx
@@ -0,0 +1,46 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import { MoviesCarouselArrow } from '.'
+
+describe('MoviesCarouselArrow component', () => {
+  it('should render with success', () => {
+    const { container } = render(
+      <MoviesCarouselArrow
+        orientation="left"
+        handleClick={() => {}}
+      />
+    )
+
+    const $arrowButton = screen.getByTestId('button-prev-movies')
+    expect($arrowButton).toHaveClass('prevButton')
+    expect(container.querySelector('svg')).toBeInTheDocument()
+  })
+
+  it('should render with next icon SVG', () => {
+    const { container } = render(
+      <MoviesCarouselArrow
+        orientation="right"
+        handleClick={() => {}}
+      />
+    )
+
+    const $arrowButton = screen.getByTestId('button-next-movies')
+    expect($arrowButton).toHaveClass('nextButton')
+    expect(container.querySelector('svg')).toBeInTheDocument()
+  })
+
+  it('should execute a function on click', () => {
+    const handleClickMock = jest.fn()
+
+    render(
+      <MoviesCarouselArrow
+        orientation="right"
+        handleClick={() => handleClickMock()}
+      />
+    )
+
+    const $arrowButton = screen.getByTestId('button-next-movies')
+    fireEvent.click($arrowButton)
+
+    expect(handleClickMock).toHaveBeenCalled()
+  })
+})
