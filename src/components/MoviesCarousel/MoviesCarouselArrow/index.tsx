541e45fe2898de2a35d27747c6bfe4128064db68
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-28 12:34:33 -0300

    Add visible prop

--- src/components/MoviesCarousel/MoviesCarouselArrow/index.tsx
+++ src/components/MoviesCarousel/MoviesCarouselArrow/index.tsx
@@ -1,20 +1,22 @@
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md'
 
 import styles from './styles.module.scss'
 
 interface IArrowProps {
   orientation: 'left' | 'right'
+  visible?: boolean
   handleClick: () => void
 }
 
-export function MoviesCarouselArrow ({ orientation, handleClick }: IArrowProps) {
+export function MoviesCarouselArrow ({ orientation, visible, handleClick }: IArrowProps) {
   return (
     <button
       className={`${orientation === 'left' ? styles.prevButton : styles.nextButton}`}
       data-testid={`${orientation === 'left' ? 'button-prev-movies' : 'button-next-movies'}`}
+      style={{ display: visible ? 'flex' : 'none' }}
       onClick={handleClick}
     >
-      {orientation === 'left' ? <IoIosArrowBack /> : <IoIosArrowForward />}
+      {orientation === 'left' ? <MdKeyboardArrowLeft /> : <MdKeyboardArrowRight />}
     </button>
   )
 }
