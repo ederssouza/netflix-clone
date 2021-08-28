477fb411d8881bdcd1e6485ea630292c1591346e
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-28 13:02:59 -0300

    Adjust coverage test

--- src/components/MoviesCarousel/MoviesCarousel.spec.tsx
+++ src/components/MoviesCarousel/MoviesCarousel.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen, fireEvent } from '@testing-library/react'
 import { MoviesCarousel } from '.'
 import { movies } from '../../tests/mocks/movies'
-import { offsetAfterAll, offsetBeforeAll } from '../../tests/utils/offset'
+import { offsetAfterAll, offsetBeforeAll } from '../../tests/utils/scrollAttributes'
 
 describe('MoviesCarousel component', () => {
   beforeAll(() => offsetBeforeAll())
@@ -28,17 +28,41 @@ describe('MoviesCarousel component', () => {
     expect($containerList.scrollLeft).toEqual(200)
   })
 
-  it('should move the scroll movie list when click prev button', () => {
+  it('should move the scroll movie list when click pre button', () => {
     render(<MoviesCarousel title="Adventure" movies={movies} />)
 
     const $containerList = screen.getByTestId('movie-list')
-    const $buttonNextMovies = screen.getByTestId('button-next-movies')
     const $buttonPrevMovies = screen.getByTestId('button-prev-movies')
+    const $buttonNextMovies = screen.getByTestId('button-next-movies')
 
     fireEvent.click($buttonNextMovies)
     fireEvent.click($buttonPrevMovies)
 
     expect($containerList.scrollLeft).toEqual(0)
+  })
+
+  it('should move the scroll movie list when clicking prev and next buttons', () => {
+    render(<MoviesCarousel title="Adventure" movies={movies} />)
+
+    const $containerList = screen.getByTestId('movie-list')
+    const $buttonPrevMovies = screen.getByTestId('button-prev-movies')
+    const $buttonNextMovies = screen.getByTestId('button-next-movies')
+
+    // start scroll position
+    fireEvent.scroll($containerList)
+    fireEvent.click($buttonNextMovies) // 200
+    fireEvent.click($buttonNextMovies) // 400
+    fireEvent.click($buttonNextMovies) // 600
+
+    // middle scroll position
+    fireEvent.scroll($containerList)
+    fireEvent.click($buttonNextMovies) // 800
+
+    // end scroll position
+    fireEvent.scroll($containerList)
+    fireEvent.click($buttonPrevMovies) // 600
+
+    expect($containerList.scrollLeft).toEqual(600)
   })
 
   afterAll(() => offsetAfterAll())
