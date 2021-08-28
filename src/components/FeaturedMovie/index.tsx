336b09336f59131d5e7a7e18e26639f94212f407
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-28 17:24:06 -0300

    Add component basic structure

--- src/components/FeaturedMovie/index.tsx
+++ src/components/FeaturedMovie/index.tsx
@@ -1,19 +1,30 @@
 import { IoMdPlay, IoMdInformationCircleOutline } from 'react-icons/io'
 import { Button } from '../Button'
 
+import styles from './styles.module.scss'
+
 export function FeaturedMovie () {
   return (
-    <div>
-      FeaturedMovie component
+    <div
+      className={styles.container}
+      style={{
+        backgroundImage: 'url("http://4.bp.blogspot.com/-5tiOMxe6rSs/UkofuptCcVI/AAAAAAAAHXk/TkY1r2cYM6M/s1600/breaking-bad.jpg")'
+      }}
+    >
+      <div className={styles.info}>
+        <div className={styles.infoText}>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae aliquam quos deleniti fugit fuga neque quisquam accusamus dignissimos.</p>
+        </div>
 
-      <div>
-        <Button color="primary" icon={<IoMdPlay />}>
-          Assistir
-        </Button>
+        <div className={styles.infoActions}>
+          <Button color="primary" icon={<IoMdPlay />}>
+            Assistir
+          </Button>
 
-        <Button color="secondary" icon={<IoMdInformationCircleOutline />}>
-          Mais informações
-        </Button>
+          <Button color="secondary" icon={<IoMdInformationCircleOutline />}>
+            Mais informações
+          </Button>
+        </div>
       </div>
     </div>
   )
