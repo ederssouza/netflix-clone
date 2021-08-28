a00d13cf541d299e23cea553f839f67fbb476d24
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-28 18:00:24 -0300

    Implement handleScroll function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,25 @@
 import Image from 'next/image'
+import { useState, useEffect } from 'react'
 
 import styles from './styles.module.scss'
 
 export function Header () {
+  const [fixedClass, setFixedClass] = useState(styles.container)
+
+  useEffect(() => {
+    function handleScroll () {
+      const pageYOffset = window.pageYOffset
+      const fixedClassValue = pageYOffset > 0 ? `${styles.container} ${styles.containerFillBackground}` : styles.container
+      setFixedClass(fixedClassValue)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <header className={styles.container}>
+    <header className={fixedClass}>
       <Image
         src="/assets/img/netflix.png"
         width="92.5px"
