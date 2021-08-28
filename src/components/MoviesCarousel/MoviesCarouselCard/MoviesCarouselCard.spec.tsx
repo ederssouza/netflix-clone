04f652889b50be82b6455a51292e77be2c19d4d7
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-28 10:39:36 -0300

    Remove dead code

--- src/components/MoviesCarousel/MoviesCarouselCard/MoviesCarouselCard.spec.tsx
+++ src/components/MoviesCarousel/MoviesCarouselCard/MoviesCarouselCard.spec.tsx
@@ -1,16 +1,14 @@
 import { render, screen } from '@testing-library/react'
 import { MoviesCarouselCard } from '.'
-
 import { movies } from '../../../tests/mocks/movies'
 
 describe('MoviesCarouselCard component', () => {
   it('should render with success', () => {
     const movie = movies[0]
-    const { container, debug } = render(<MoviesCarouselCard movie={movie} />)
+    const { container } = render(<MoviesCarouselCard movie={movie} />)
     const $movieCardImage = screen.getByAltText(movie.title)
 
     expect(container.querySelector('a')).toHaveAttribute('title', movie.title)
     expect($movieCardImage).toHaveAttribute('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7')
-    debug()
   })
 })
