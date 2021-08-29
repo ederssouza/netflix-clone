95e33727a50b45fef7b66e20b93428ae04ed06d9
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-28 21:02:42 -0300

    Add more tests

--- src/components/Header/Header.spec.tsx
+++ src/components/Header/Header.spec.tsx
@@ -8,6 +8,24 @@ describe('Header component', () => {
   })
 
   // TODO: should add `containerFillBackground` CSS class when pageYOffset is greater than zero
+  it('should add `containerFillBackground` CSS class when pageYOffset is greater than zero', () => {
+    const { container } = render(<Header />)
+
+    Object.defineProperty(window, 'pageYOffset', { value: 0 })
+
+    const $header = screen.getByTestId('main-header')
+
+    console.log(container.innerHTML)
+    console.log(window.pageYOffset)
+
+    Object.defineProperty(window, 'pageYOffset', { value: 100 })
+
+    console.log(container.innerHTML)
+    console.log(window.pageYOffset)
+  })
 
   // TODO: should remove `containerFillBackground` CSS class when pageYOffset equals zero
+  it('should remove `containerFillBackground` CSS class when pageYOffset equals zero', () => {
+
+  })
 })
