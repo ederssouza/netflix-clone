4f1348c20d2f26953105a6e70ea1c47dc938735f
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-29 10:48:01 -0300

    Add Genre component

---
+++ src/pages/genre/[id].tsx
@@ -0,0 +1,37 @@
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+
+import { Header } from '../../components/Header'
+
+import styles from './styles.module.scss'
+
+interface IGenreProps {
+  id: string
+}
+
+export default function Genre ({ id }: IGenreProps) {
+  return (
+    <>
+      <Head>
+        <title>GENRE_NAME ID | Netflix</title>
+        <meta name="description" content="..." />
+        <link rel="icon" href="/assets/img/favicon.ico" />
+      </Head>
+
+      <main className={styles.container}>
+        <Header />
+        <h1>Genre {id}</h1>
+      </main>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { id } = query
+
+  return {
+    props: {
+      id
+    }
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 
-import { FeaturedMovie } from '../components/FeaturedMovie'
 import { Header } from '../components/Header'
+import { FeaturedMovie } from '../components/FeaturedMovie'
 import { MoviesCarousel } from '../components/MoviesCarousel'
 import { Footer } from '../components/Footer'
 
