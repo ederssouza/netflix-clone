951c1cbf591ee63114e75edb1c0371a36d8a874a
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-29 13:02:14 -0300

    Create Profile component

---
+++ src/components/Header/Profile/index.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './styles.module.scss'
+
+export function Profile () {
+  return (
+    <div className={styles.profile}>
+      <Image src="/assets/img/avatar.png" alt="Avatar" width="32" height="32" />
+    </div>
+  )
+}
