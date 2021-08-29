585de161861609292b1e3312d05914cc6a2f75f6
Eder Sampaio <edersampaio@outlook.com.br>
2021-08-29 10:49:30 -0300

    Add Logo component tests

---
+++ src/components/Header/Logo/Logo.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react'
+import { Logo } from '.'
+
+describe('Logo component', () => {
+  it('should render with success', () => {
+    render(<Logo />)
+
+    expect(screen.getByTitle('Início')).toBeInTheDocument()
+    expect(screen.getByAltText('Netflix logo')).toBeInTheDocument()
+  })
+
+  it('should render with valid href attribute', () => {
+    render(<Logo />)
+
+    const $logo = screen.getByTestId('logo')
+
+    expect($logo).toHaveAttribute('href', '/')
+  })
+})
