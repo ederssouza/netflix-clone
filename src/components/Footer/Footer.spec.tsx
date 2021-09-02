27d36471c52dc2a91b3eba47cd35ee3ff062467f
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-02 18:40:31 -0300

    Update Footer tests

--- src/components/Footer/Footer.spec.tsx
+++ src/components/Footer/Footer.spec.tsx
@@ -2,8 +2,26 @@ import { render, screen } from '@testing-library/react'
 import { Footer } from '.'
 
 describe('Footer component', () => {
-  it('should render with success', () => {
+  it('should render with success the social media links', () => {
     render(<Footer />)
-    expect(screen.getByText('Footer component')).toBeInTheDocument()
+
+    const $footer = screen.getByTestId('footer')
+
+    expect($footer.querySelector('.socialMedia svg')).toBeInTheDocument()
+  })
+
+  it('should render with success the links', () => {
+    render(<Footer />)
+
+    expect(screen.getByText('Idioma e legendas')).toBeInTheDocument()
+    expect(screen.getByText('Audiodescrição')).toBeInTheDocument()
+    expect(screen.getByText('Centro de ajuda')).toBeInTheDocument()
+    expect(screen.getByText('Cartão pré-pago')).toBeInTheDocument()
+  })
+
+  it('should render copyright', () => {
+    render(<Footer />)
+
+    expect(screen.getByText(/1997-2021/)).toBeInTheDocument()
   })
 })
