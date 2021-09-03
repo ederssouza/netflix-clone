09ad4d78055be3ce14b087c255a6d4a0a43c4964
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-03 09:52:27 -0300

    Adjust Footer component tests

--- src/components/Footer/Footer.spec.tsx
+++ src/components/Footer/Footer.spec.tsx
@@ -2,12 +2,29 @@ import { render, screen } from '@testing-library/react'
 import { Footer } from '.'
 
 describe('Footer component', () => {
-  it('should render with success the social media links', () => {
+  it('should render with success all social media links', () => {
     render(<Footer />)
 
-    const $footer = screen.getByTestId('footer')
+    const $facebook = screen.getByTitle('Facebook')
+    const $istagram = screen.getByTitle('Instagram')
+    const $twitter = screen.getByTitle('Twitter')
+    const $youTube = screen.getByTitle('YouTube')
 
-    expect($footer.querySelector('.socialMedia svg')).toBeInTheDocument()
+    expect($facebook).toBeInTheDocument()
+    expect($facebook).toHaveAttribute('href', 'https://www.facebook.com/netflixbrasil')
+    expect($facebook.querySelector('svg')).toBeInTheDocument()
+
+    expect($istagram).toBeInTheDocument()
+    expect($istagram).toHaveAttribute('href', 'https://www.instagram.com/NetflixBrasil')
+    expect($istagram.querySelector('svg')).toBeInTheDocument()
+
+    expect($twitter).toBeInTheDocument()
+    expect($twitter).toHaveAttribute('href', 'https://twitter.com/NetflixBrasil')
+    expect($twitter.querySelector('svg')).toBeInTheDocument()
+
+    expect($youTube).toBeInTheDocument()
+    expect($youTube).toHaveAttribute('href', 'https://www.youtube.com/user/NetflixBRA')
+    expect($youTube.querySelector('svg')).toBeInTheDocument()
   })
 
   it('should render with success the links', () => {
@@ -22,6 +39,6 @@ describe('Footer component', () => {
   it('should render copyright', () => {
     render(<Footer />)
 
-    expect(screen.getByText(/1997-2021/)).toBeInTheDocument()
+    expect(screen.getByText(/1997-\d{4}\s+Netflix,\s+Inc/)).toBeInTheDocument()
   })
 })
