928f53084383b53fe41d0d41597c890738ca1e40
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-03 15:56:06 -0300

    Implement handleContainerMarginTop function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useEffect, useRef, useState } from 'react'
 
 import { Header } from '../components/Header'
 import { FeaturedMovie } from '../components/FeaturedMovie'
@@ -8,14 +9,30 @@ import { Footer } from '../components/Footer'
 import common from '../styles/common.module.scss'
 import styles from './home.module.scss'
 
+const movies = Array.from({ length: 20 }).map((_, index) => {
+  return {
+    id: index + 1,
+    title: 'Movie title',
+    image: 'https://occ-0-626-1123.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQ1n7nMsHQU4eAT8s7OtRNBTkRt8KcVP9M0q5ZnZRppX0WwulOFBD6wHx0U4pOga86psNHvwzBvJah7ey3BpIgbMOeE.webp?r=252'
+  }
+})
+
 export default function Home () {
-  const movies = Array.from({ length: 20 }).map((_, index) => {
-    return {
-      id: index + 1,
-      title: 'Movie title',
-      image: 'https://occ-0-626-1123.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQ1n7nMsHQU4eAT8s7OtRNBTkRt8KcVP9M0q5ZnZRppX0WwulOFBD6wHx0U4pOga86psNHvwzBvJah7ey3BpIgbMOeE.webp?r=252'
-    }
-  })
+  const refMoviesCarousel = useRef(null)
+  const [containerMarginTop, setContainerMarginTop] = useState('0px')
+
+  function handleContainerMarginTop () {
+    setTimeout(() => {
+      const containerheight = refMoviesCarousel.current.clientHeight
+      setContainerMarginTop(`-${containerheight}px`)
+    }, 10)
+  }
+
+  useEffect(() => {
+    handleContainerMarginTop()
+    window.addEventListener('resize', handleContainerMarginTop)
+    return () => window.removeEventListener('resize', handleContainerMarginTop)
+  }, [])
 
   return (
     <>
@@ -29,8 +46,15 @@ export default function Home () {
         <Header />
         <FeaturedMovie />
 
-        <div className={common.moviesContainer}>
-          <MoviesCarousel title="Populares Netflix" movies={movies} />
+        <div
+          className={common.moviesContainer}
+          style={{ marginTop: containerMarginTop }}
+          data-testid="movies-container"
+        >
+          <div ref={refMoviesCarousel}>
+            <MoviesCarousel title="Populares Netflix" movies={movies} />
+          </div>
+
           <MoviesCarousel title="Em alta" movies={movies} />
           <MoviesCarousel title="Lançamentos" movies={movies} />
           <MoviesCarousel title="Assistir novamente" movies={movies} />
