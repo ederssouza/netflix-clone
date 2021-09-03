10c9907d8aba22e9a2b6ada2c3f654c1e7676e20
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-03 09:52:55 -0300

    Add break lines and add href on social medias

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,37 +6,79 @@ export function Footer () {
     <footer className={styles.footer} data-testid="footer">
       <div className={styles.container}>
         <ul className={styles.socialMedia}>
-          <li><a href="#" title="Facebook"><ImFacebook2 /></a></li>
-          <li><a href="#" title="Instagram"><ImInstagram /></a></li>
-          <li><a href="#" title="Twitter"><ImTwitter /></a></li>
-          <li><a href="#" title="YouTube"><ImYoutube /></a></li>
+          <li>
+            <a href="https://www.facebook.com/netflixbrasil" title="Facebook">
+              <ImFacebook2 />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/NetflixBrasil" title="Instagram">
+              <ImInstagram />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/NetflixBrasil" title="Twitter">
+              <ImTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/user/NetflixBRA" title="YouTube">
+              <ImYoutube />
+            </a>
+          </li>
         </ul>
       </div>
 
       <div className={styles.container}>
         <ul className={styles.footerList}>
-          <li><a href="#" title="Idioma e legendas">Idioma e legendas</a></li>
-          <li><a href="#" title="Imprensa">Imprensa</a></li>
-          <li><a href="#" title="Privacidade">Privacidade</a></li>
-          <li><a href="#" title="Entre em contato">Entre em contato</a></li>
+          <li>
+            <a href="#" title="Idioma e legendas">Idioma e legendas</a>
+          </li>
+          <li>
+            <a href="#" title="Imprensa">Imprensa</a>
+          </li>
+          <li>
+            <a href="#" title="Privacidade">Privacidade</a>
+          </li>
+          <li>
+            <a href="#" title="Entre em contato">Entre em contato</a>
+          </li>
         </ul>
 
         <ul className={styles.footerList}>
-          <li><a href="#" title="Audiodescrição">Audiodescrição</a></li>
-          <li><a href="#" title="Relações com investidores">Relações com investidores</a></li>
-          <li><a href="#" title="Avisos legais">Avisos legais</a></li>
+          <li>
+            <a href="#" title="Audiodescrição">Audiodescrição</a>
+          </li>
+          <li>
+            <a href="#" title="Relações com investidores">Relações com investidores</a>
+          </li>
+          <li>
+            <a href="#" title="Avisos legais">Avisos legais</a>
+          </li>
         </ul>
 
         <ul className={styles.footerList}>
-          <li><a href="#" title="Centro de ajuda">Centro de ajuda</a></li>
-          <li><a href="#" title="Carreiras">Carreiras</a></li>
-          <li><a href="#" title="Preferências de cookies">Preferências de cookies</a></li>
+          <li>
+            <a href="#" title="Centro de ajuda">Centro de ajuda</a>
+          </li>
+          <li>
+            <a href="#" title="Carreiras">Carreiras</a>
+          </li>
+          <li>
+            <a href="#" title="Preferências de cookies">Preferências de cookies</a>
+          </li>
         </ul>
 
         <ul className={styles.footerList}>
-          <li><a href="#" title="Cartão pré-pago">Cartão pré-pago</a></li>
-          <li><a href="#" title="Termos de uso">Termos de uso</a></li>
-          <li><a href="#" title="Informações corporativas">Informações corporativas</a></li>
+          <li>
+            <a href="#" title="Cartão pré-pago">Cartão pré-pago</a>
+          </li>
+          <li>
+            <a href="#" title="Termos de uso">Termos de uso</a>
+          </li>
+          <li>
+            <a href="#" title="Informações corporativas">Informações corporativas</a>
+          </li>
         </ul>
       </div>
 
