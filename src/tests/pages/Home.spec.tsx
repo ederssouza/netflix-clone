2ac5de40ffa18420125b3c36ae582863488d77a1
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-03 15:53:30 -0300

    Adjust Home tests

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,9 +1,14 @@
-import { render, screen } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
 import Home from '../../pages/index'
 
 describe('Home page component', () => {
   it('should render with success', () => {
+    jest.useFakeTimers()
+
     render(<Home />)
+
+    act(() => jest.advanceTimersByTime(10))
+
     expect(screen.getByAltText('Netflix logo')).toBeInTheDocument()
   })
 })
