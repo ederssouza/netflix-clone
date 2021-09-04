492bb8c6f1b772fa3eff02b62f2d5039ece4f2e2
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-04 11:22:17 -0300

    Create MenuItems component

---
+++ src/components/Header/Menu/MenuItems.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import styles from './styles.module.scss'
+
+interface IMenuItemProps {
+  href: string
+  title: string
+}
+
+export interface IMenuProps {
+  items: IMenuItemProps[]
+}
+
+export function MenuItems ({ items }: IMenuProps) {
+  const router = useRouter()
+  const asPath = router?.asPath || null
+
+  return (
+    <ul className={styles.navbarMenu}>
+      {items.map(link => {
+        const className = asPath === link.href
+          ? styles.navbarMenuItemActive
+          : null
+
+        return (
+          <li
+            key={link.title}
+            className={link.href === asPath ? className : null}
+          >
+            <Link href={link.href}>
+              <a title={link.title}>{link.title}</a>
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
