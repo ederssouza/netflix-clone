836d489cce368736367ea6e6baa24e519af9a373
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-05 11:59:25 -0300

    Implement carousel card details

--- src/components/MoviesCarousel/MoviesCarouselCard/index.tsx
+++ src/components/MoviesCarousel/MoviesCarouselCard/index.tsx
@@ -1,3 +1,6 @@
+import { IoMdPlay } from 'react-icons/io'
+import { TiInfoLarge } from 'react-icons/ti'
+
 import styles from './styles.module.scss'
 
 export interface IMovie {
@@ -17,6 +20,23 @@ export function MoviesCarouselCard ({ movie }: IMovieProps) {
       data-testid="movies-carousel-card"
     >
       <img src={movie.image} alt={movie.title} />
+
+      <div className={styles.containerDetails}>
+        <ul className={styles.actions}>
+          <li className={styles.actionItemActive}><a href="#" title=""><IoMdPlay /></a></li>
+          <li className={styles.actionItem}><a href="#" title=""><TiInfoLarge /></a></li>
+        </ul>
+
+        <span className={`${styles.relevant} ${styles.relevantGreen}`}>
+          87% relevante
+        </span>
+
+        <ul className={styles.genres}>
+          <li>Drama</li>
+          <li>Terror</li>
+          <li>Suspense</li>
+        </ul>
+      </div>
     </div>
   )
 }
