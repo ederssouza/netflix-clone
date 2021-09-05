70acaa9a991c39ca90e16a23545937d2a6bf1127
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-05 16:51:29 -0300

    Implement Genre view component

--- src/pages/genre/[id].tsx
+++ src/pages/genre/[id].tsx
@@ -1,15 +1,44 @@
 import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import { useEffect, useRef, useState } from 'react'
 
 import { Header } from '../../components/Header'
+import { FeaturedMovie } from '../../components/FeaturedMovie'
+import { MoviesCarousel } from '../../components/MoviesCarousel'
+import { Footer } from '../../components/Footer'
 
-import styles from './styles.module.scss'
+import common from '../../styles/common.module.scss'
+import styles from '../home.module.scss'
 
 interface IGenreProps {
   id: string
 }
 
+const movies = Array.from({ length: 20 }).map((_, index) => {
+  return {
+    id: index + 1,
+    title: 'Movie title',
+    image: 'https://occ-0-626-1123.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQ1n7nMsHQU4eAT8s7OtRNBTkRt8KcVP9M0q5ZnZRppX0WwulOFBD6wHx0U4pOga86psNHvwzBvJah7ey3BpIgbMOeE.webp?r=252'
+  }
+})
+
 export default function Genre ({ id }: IGenreProps) {
+  const refMoviesCarousel = useRef(null)
+  const [containerMarginTop, setContainerMarginTop] = useState('0px')
+
+  function handleContainerMarginTop () {
+    setTimeout(() => {
+      const containerheight = refMoviesCarousel.current.clientHeight
+      setContainerMarginTop(`-${containerheight}px`)
+    }, 10)
+  }
+
+  useEffect(() => {
+    handleContainerMarginTop()
+    window.addEventListener('resize', handleContainerMarginTop)
+    return () => window.removeEventListener('resize', handleContainerMarginTop)
+  }, [])
+
   return (
     <>
       <Head>
@@ -20,7 +49,30 @@ export default function Genre ({ id }: IGenreProps) {
 
       <main className={styles.container}>
         <Header />
-        <h1>Genre {id}</h1>
+        <FeaturedMovie movie={{
+          id: 1,
+          background: 'http://4.bp.blogspot.com/-5tiOMxe6rSs/UkofuptCcVI/AAAAAAAAHXk/TkY1r2cYM6M/s1600/breaking-bad.jpg',
+          title: 'Movie title',
+          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae aliquam quos deleniti fugit fuga neque quisquam accusamus dignissimos.'
+        }} />
+
+        <div
+          className={common.moviesContainer}
+          style={{ marginTop: containerMarginTop }}
+          data-testid="movies-container"
+        >
+          <MoviesCarousel
+            ref={refMoviesCarousel}
+            title="Populares Netflix"
+            movies={movies}
+          />
+          <MoviesCarousel title="Em alta" movies={movies} />
+          <MoviesCarousel title="Lançamentos" movies={movies} />
+          <MoviesCarousel title="Assistir novamente" movies={movies} />
+          <MoviesCarousel title="Documentários" movies={movies} />
+        </div>
+
+        <Footer />
       </main>
     </>
   )
