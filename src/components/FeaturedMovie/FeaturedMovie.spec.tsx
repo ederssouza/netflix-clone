5474d7f0b963cd8656df87101920a40d0125bbb8
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-05 17:15:59 -0300

    Remove dead code

--- src/components/FeaturedMovie/FeaturedMovie.spec.tsx
+++ src/components/FeaturedMovie/FeaturedMovie.spec.tsx
@@ -1,21 +1,15 @@
 import { render, screen } from '@testing-library/react'
 import { FeaturedMovie } from '.'
-
-const movie = {
-  id: 1,
-  background: 'http://4.bp.blogspot.com/-5tiOMxe6rSs/UkofuptCcVI/AAAAAAAAHXk/TkY1r2cYM6M/s1600/breaking-bad.jpg',
-  title: 'Movie title',
-  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae aliquam quos deleniti fugit fuga neque quisquam accusamus dignissimos.'
-}
+import { movies } from '../../tests/mocks/movies'
 
 describe('FeaturedMovie component', () => {
   it('should render with success', () => {
-    render(<FeaturedMovie movie={movie} />)
+    render(<FeaturedMovie movie={movies[0]} />)
 
     const $featuredMovie = screen.getByTestId('featured-movie')
 
-    expect(screen.getByText(movie.title)).toBeInTheDocument()
+    expect(screen.getByText(movies[0].title)).toBeInTheDocument()
     expect(screen.getByText(/Lorem ipsum/i)).toBeInTheDocument()
-    expect($featuredMovie).toHaveAttribute('style', `background-image: url(${movie.background});`)
+    expect($featuredMovie).toHaveAttribute('style', `background-image: url(${movies[0].image});`)
   })
 })
