e66c78efb428f4296613278117cf1a379d44351d
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-05 09:44:49 -0300

    Adjust tests

--- src/components/FeaturedMovie/FeaturedMovie.spec.tsx
+++ src/components/FeaturedMovie/FeaturedMovie.spec.tsx
@@ -1,9 +1,21 @@
 import { render, screen } from '@testing-library/react'
 import { FeaturedMovie } from '.'
 
+const movie = {
+  id: 1,
+  background: 'http://4.bp.blogspot.com/-5tiOMxe6rSs/UkofuptCcVI/AAAAAAAAHXk/TkY1r2cYM6M/s1600/breaking-bad.jpg',
+  title: 'Movie title',
+  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae aliquam quos deleniti fugit fuga neque quisquam accusamus dignissimos.'
+}
+
 describe('FeaturedMovie component', () => {
   it('should render with success', () => {
-    render(<FeaturedMovie />)
+    render(<FeaturedMovie movie={movie} />)
+
+    const $featuredMovie = screen.getByTestId('featured-movie')
+
+    expect(screen.getByText(movie.title)).toBeInTheDocument()
     expect(screen.getByText(/Lorem ipsum/i)).toBeInTheDocument()
+    expect($featuredMovie).toHaveAttribute('style', `background-image: url(${movie.background});`)
   })
 })
