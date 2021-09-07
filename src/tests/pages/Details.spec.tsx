55733a6cf3daa04697841a87113df01495b1cf90
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-07 11:37:06 -0300

    Add Details test

---
+++ src/tests/pages/Details.spec.tsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react'
+import Details, { getServerSideProps } from '../../pages/details'
+
+describe('Details page component', () => {
+  it('should redirect to home when URL does not have `id` param', async () => {
+    const response = await getServerSideProps({} as any)
+
+    render(<Details />)
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        redirect: expect.objectContaining({
+          destination: '/',
+          permanent: true
+        })
+      })
+    )
+  })
+})
