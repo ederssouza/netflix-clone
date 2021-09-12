20d78bc1b8d23b08fb50dd7e1d234bf52ef39153
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-12 11:18:11 -0300

    Import TMDB_BASE_URL_IMAGE constant

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,5 @@
+import { TMDB_BASE_URL_IMAGE } from '../services/tmdb'
+
 export function timeConvert (num: number) {
   if (!num) return
 
@@ -13,16 +15,19 @@ export function isValidArray (value: any) {
 }
 
 export function normalizeMoviePayload (data: any) {
-  if (!data) return
+  if (!data) return {}
 
+  const backdrop_path = data?.backdrop_path
+    ? `${TMDB_BASE_URL_IMAGE}${data.backdrop_path}`
+    : '/assets/img/banner.jpeg'
   const release_date = data?.release_date || data?.first_air_date
   const original_language = data?.original_language || (data?.episode_run_time?.length ? data?.episode_run_time[0] : null)
   const runtime = data?.runtime || (data?.episode_run_time?.length ? data?.episode_run_time[0] : null)
 
   return {
     title: data?.title || data?.name,
-    overview: data?.overview,
-    backdrop_path: data?.backdrop_path,
+    overview: data?.overview || 'Nenhum resumo disponível',
+    backdrop_path,
     vote_average: data?.vote_average,
     release_date: release_date ? new Date(release_date).getFullYear() : null,
     original_language: original_language?.toUpperCase(),
