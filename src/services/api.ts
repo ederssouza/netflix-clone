69522428b71835ab9fc4963ceb44b44fb94387be
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-12 18:30:17 -0300

    Implement api service module

---
+++ src/services/api.ts
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+interface IGetById {
+  type: string
+  id: string
+}
+
+interface ISearch {
+  query: string
+  page?: number
+}
+
+export const axiosInstance = axios.create({
+  baseURL: 'http://localhost:3000/api/tmdb'
+})
+
+export const api = {
+  getDetailsById ({ type, id }: IGetById) {
+    return axiosInstance.get(`/details/${type}/${id}`)
+  },
+
+  getCreditsById ({ type, id }: IGetById) {
+    return axiosInstance.get(`/credits/${type}/${id}`)
+  },
+
+  getWatchProvidersById ({ type, id }: IGetById) {
+    return axiosInstance.get(`/providers/${type}/${id}`)
+  },
+
+  search ({ query, page = 1 }: ISearch) {
+    return axiosInstance.get('/search', {
+      params: { query, page }
+    })
+  }
+}
