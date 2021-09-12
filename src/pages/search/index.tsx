32442adb6e147ea5e3f548d5f038110506ef89b1
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-12 20:07:22 -0300

    Implement Search page component

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,19 +1,42 @@
 import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 
-// import { MoviesCarouselCard } from '../../components/MoviesCarousel/MoviesCarouselCard'
+import { IMovie } from '../../@types'
 import { CardsSkeletonLoader } from '../../components/CardsSkeletonLoader'
 import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
+import { MoviesCarouselCard } from '../../components/MoviesCarousel/MoviesCarouselCard'
+import { api } from '../../services/api'
+import { normalizeMoviePayload } from '../../utils/functions'
 import styles from './styles.module.scss'
-
-// import { movies } from '../../tests/mocks/movies'
 
 interface ISearchProps {
   q?: string
 }
 
 export default function Search ({ q }: ISearchProps) {
+  const [movies, setMovies] = useState([])
+  const [statusRequest, setStatusRequest] = useState('loading')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setStatusRequest('loading')
+
+        const res = await api.search({ query: String(q), page: 1 })
+        const movies = res.data.results.map((movie: IMovie) => normalizeMoviePayload(movie))
+
+        setMovies(movies)
+        setTimeout(() => setStatusRequest('success'), 500)
+      } catch (error) {
+        setStatusRequest('error')
+      }
+    }
+
+    fetchData()
+  }, [q])
+
   return (
     <>
       <Head>
@@ -26,18 +49,26 @@ export default function Search ({ q }: ISearchProps) {
         <Header />
         <h1>Buscar por: {q}</h1>
 
-        {/* TODO: loader status */}
-        <div>
-          <CardsSkeletonLoader />
-          <CardsSkeletonLoader />
-          <CardsSkeletonLoader />
-          <CardsSkeletonLoader />
-        </div>
+        {statusRequest === 'loading' && (
+          <div>
+            <CardsSkeletonLoader />
+            <CardsSkeletonLoader />
+            <CardsSkeletonLoader />
+            <CardsSkeletonLoader />
+          </div>
+        )}
 
-        {/* TODO: success status */}
-        {/* <div className={styles.grid}>
-          {movies.map(movie => <MoviesCarouselCard key={movie.id} movie={movie} />)}
-        </div> */}
+        {statusRequest === 'error' && (
+          <div>
+            <h1>Ocorreu um erro</h1>
+          </div>
+        )}
+
+        {statusRequest === 'success' && (
+          <div className={styles.grid}>
+            {movies.map(movie => <MoviesCarouselCard key={movie.id} movie={movie} />)}
+          </div>
+        )}
       </main>
 
       <Footer />
