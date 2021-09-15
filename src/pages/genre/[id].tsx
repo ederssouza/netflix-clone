9daeb728b0f0d5706145c04623cae81104c0ea6c
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-15 19:10:24 -0300

    Implement GenreById page component

--- src/pages/genre/[id].tsx
+++ src/pages/genre/[id].tsx
@@ -1,40 +1,92 @@
 import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import { useEffect, useRef, useState } from 'react'
 
-import { FeaturedMovie } from '../../components/FeaturedMovie'
+import { IMovie } from '../../@types'
+import { CardsSkeletonLoader } from '../../components/CardsSkeletonLoader'
 import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
-import { MoviesCarousel } from '../../components/MoviesCarousel'
-import { MoviesContainer } from '../../components/MoviesContainer'
-import { movies } from '../../tests/mocks/tmdb'
+import { MoviesCarouselCard } from '../../components/MoviesCarousel/MoviesCarouselCard'
+import { useOnScreen } from '../../hooks/useOnScreen'
+import { api } from '../../services/api'
+import { normalizeMoviePayload } from '../../utils/functions'
+import styles from './styles.module.scss'
 
 interface IGenreProps {
   id: string
 }
 
 export default function GenreById ({ id }: IGenreProps) {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [movies, setMovies] = useState([])
+  const [hasMore, setHasMore] = useState(true)
+  const [statusRequest, setStatusRequest] = useState('loading')
+  const footerRef = useRef<HTMLDivElement | null>(null)
+  const { isIntersecting } = useOnScreen(footerRef)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        currentPage === 1 ? setStatusRequest('loading') : setStatusRequest('loadmore')
+
+        const res = await api.getGenreById({ type: 'tv', id: 99, page: currentPage })
+        const movies = res.data.results.map((movie: IMovie) => normalizeMoviePayload(movie))
+
+        setMovies((oldMovies) => [...oldMovies, ...movies.slice(0, 18)])
+        setHasMore(currentPage <= res.data.total_pages)
+        setTimeout(() => setStatusRequest('success'), 500)
+      } catch (error) {
+        setStatusRequest('error')
+      }
+    }
+
+    fetchData()
+  }, [currentPage])
+
+  useEffect(() => {
+    if (isIntersecting && hasMore) {
+      setCurrentPage(oldCurrentPage => oldCurrentPage + 1)
+    }
+  }, [isIntersecting, hasMore])
+
   return (
     <>
       <Head>
-        <title>Gênero {id} | Netflix</title>
+        <title>Busca | Netflix</title>
         <meta name="description" content="..." />
         <link rel="icon" href="/assets/img/favicon.ico" />
       </Head>
 
-      <main>
+      <main className={styles.container}>
         <Header />
-        <FeaturedMovie category={`Gênero ${id}`} movie={movies[0]} />
+        <h1>[GENRE_NAME] {currentPage}</h1>
 
-        <MoviesContainer>
-          <MoviesCarousel title="Populares Netflix" movies={movies} />
-          <MoviesCarousel title="Em alta" movies={movies} />
-          <MoviesCarousel title="Lançamentos" movies={movies} />
-          <MoviesCarousel title="Assistir novamente" movies={movies} />
-          <MoviesCarousel title="Documentários" movies={movies} />
-        </MoviesContainer>
+        {(statusRequest === 'success' || statusRequest === 'loadmore') && (
+          <div className={styles.grid}>
+            {movies.map((movie, index) => <MoviesCarouselCard key={`${movie.id}${index}`} movie={movie} />)}
+          </div>
+        )}
 
-        <Footer />
+        {statusRequest === 'error' && (
+          <div>
+            {/* TODO: create component */}
+            <h1>Ocorreu um erro</h1>
+          </div>
+        )}
+
+        {(statusRequest === 'loading' || statusRequest === 'loadmore') && (
+          <div>
+            <CardsSkeletonLoader />
+            <CardsSkeletonLoader />
+            <CardsSkeletonLoader />
+            <CardsSkeletonLoader />
+          </div>
+        )}
       </main>
+
+      <div ref={footerRef}>
+        <Footer />
+      </div>
     </>
   )
 }
