0a69a83dd158673e48f222b45d77b380de776ddc
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-15 18:22:25 -0300

    Add getTrendings endpoint

---
+++ src/pages/api/tmdb/trendings/index.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { tmdbService } from '../../../../services/tmdb'
+
+export default async function getTrendings (req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const response = await tmdbService.getTrendings()
+
+    return res.status(200).json({ ...response.data })
+  } catch (error) {
+    const code = error?.response?.status
+
+    return res.status(code).json({
+      code,
+      statusText: error?.response?.statusText || '',
+      error: error?.response?.data?.status_message || ''
+    })
+  }
+}
