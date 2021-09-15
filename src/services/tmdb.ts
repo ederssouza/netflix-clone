8fbaa6e0a695f55667d67f43a05cc122e7beb66e
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-15 18:20:43 -0300

    Add more endpoints

--- src/services/tmdb.ts
+++ src/services/tmdb.ts
@@ -10,6 +10,25 @@ interface ISearch {
   page?: number
 }
 
+interface IGenres {
+  type: string
+}
+
+interface ITopRated {
+  type: string
+  page?: number
+}
+
+interface IGenreById {
+  type: string
+  id: number
+  page?: number
+}
+
+interface INetflixList {
+  page?: number
+}
+
 export const axiosInstance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
@@ -40,6 +59,46 @@ export const tmdbService = {
         language: 'pt-BR',
         query,
         include_adult: false,
+        page: Number(page)
+      }
+    })
+  },
+
+  getGenres ({ type }: IGenres) {
+    return axiosInstance.get(`/${type}/top_rated`)
+  },
+
+  getTopRated ({ type, page = 1 }: ITopRated) {
+    return axiosInstance.get(`/${type}/top_rated`, {
+      params: {
+        api_key: process.env.TMDB_API_KEY,
+        language: 'pt-BR',
+        page: Number(page)
+      }
+    })
+  },
+
+  getTrendings () {
+    return axiosInstance.get('/trending/all/week')
+  },
+
+  getGenreById ({ type, id, page = 1 }: IGenreById) {
+    return axiosInstance.get(`/discover/${type}`, {
+      params: {
+        api_key: process.env.TMDB_API_KEY,
+        with_genres: id,
+        language: 'pt-BR',
+        page: Number(page)
+      }
+    })
+  },
+
+  getNetflixList ({ page = 1 }: INetflixList) {
+    return axiosInstance.get('/discover/tv', {
+      params: {
+        api_key: process.env.TMDB_API_KEY,
+        with_network: 213,
+        language: 'pt-BR',
         page: Number(page)
       }
     })
