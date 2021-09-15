7b93a30fa1af7b3cbd8fa0291b592e27a83919c6
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-15 18:45:16 -0300

    Add new methods

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,6 +10,25 @@ interface ISearch {
   page?: number
 }
 
+interface IGenres {
+  type: string
+}
+
+interface ITopRated {
+  type: string
+  page?: number
+}
+
+interface IGenreById {
+  type: string
+  id: number
+  page?: number
+}
+
+interface INetflixList {
+  page?: number
+}
+
 export const axiosInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_APP_URL}/api/tmdb`
 })
@@ -31,5 +50,21 @@ export const api = {
     return axiosInstance.get('/search', {
       params: { query, page }
     })
-  }
+  },
+
+  getGenres ({ type }: IGenres) {},
+
+  getTopRated ({ type, page = 1 }: ITopRated) {},
+
+  getTrendings () {},
+
+  getGenreById ({ type, id, page = 1 }: IGenreById) {
+    return axiosInstance.get(`/genre/${type}/${id}`, {
+      params: {
+        page: Number(page)
+      }
+    })
+  },
+
+  getNetflixList ({ page = 1 }: INetflixList) {}
 }
