ab87878d444f9309e1253a58cec587a5e0922ab3
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-16 12:31:33 -0300

    Add MoviesContainer tests

---
+++ src/components/MoviesContainer/MoviesContainer.spec.tsx
@@ -0,0 +1,17 @@
+import { act, render } from '@testing-library/react'
+
+import { MoviesContainer } from '.'
+
+describe('MoviesContainer component', () => {
+  it('should render left arrow with success', () => {
+    jest.useFakeTimers()
+
+    render(
+      <MoviesContainer>
+        <div>content...</div>
+      </MoviesContainer>
+    )
+
+    act(() => jest.advanceTimersByTime(500))
+  })
+})
