2e41f6d2f4ffc4ba12601895648e5d6d1abe009d
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-16 17:47:45 -0300

    Add getHomeLists method

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+import { normalizeMoviePayload } from '../../utils/functions'
+
 interface IGetById {
   type: string
   id: string
@@ -80,5 +82,67 @@ export const api = {
         page: Number(page)
       }
     })
+  },
+
+  async getHomeLists () {
+    const [netflix, trendings, action, adventure, comedy, documentaries] = await Promise.all([
+      this.getNetflixList({ page: 1 }),
+      this.getTrendings({ page: 1 }),
+      this.getGenreById({ type: 'movie', id: '28', page: 2 }),
+      this.getGenreById({ type: 'movie', id: '12', page: 2 }),
+      this.getGenreById({ type: 'movie', id: '35', page: 2 }),
+      this.getGenreById({ type: 'tv', id: '99', page: 2 })
+    ])
+
+    const netflixMovieData = netflix?.data?.results || []
+    const trendingsData = trendings?.data?.results || []
+    const actionData = action?.data?.results || []
+    const adventureData = adventure?.data?.results || []
+    const comedyData = comedy?.data?.results || []
+    const documentariesData = documentaries?.data?.results || []
+
+    const netflixList = netflixMovieData.map(movie => normalizeMoviePayload({
+      ...movie,
+      media_type: 'tv',
+      genres: []
+    }))
+
+    const trendingsList = trendingsData.map(movie => normalizeMoviePayload({
+      ...movie,
+      genres: []
+    }))
+
+    const actionList = actionData.map(movie => normalizeMoviePayload({
+      ...movie,
+      media_type: 'movie',
+      genres: []
+    }))
+
+    const adventureList = adventureData.map(movie => normalizeMoviePayload({
+      ...movie,
+      media_type: 'movie',
+      genres: []
+    }))
+
+    const comedyList = comedyData.map(movie => normalizeMoviePayload({
+      ...movie,
+      media_type: 'movie',
+      genres: []
+    }))
+
+    const documentariesList = documentariesData.map(movie => normalizeMoviePayload({
+      ...movie,
+      media_type: 'tv',
+      genres: []
+    }))
+
+    return {
+      netflix: [...netflixList],
+      trendings: [...trendingsList],
+      action: [...actionList],
+      adventure: [...adventureList],
+      comedy: [...comedyList],
+      documentaries: [...documentariesList]
+    }
   }
 }
