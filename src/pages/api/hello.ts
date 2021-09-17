1292367965c62a0381e4cd24424df315ece16c3a
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-17 11:47:16 -0300

    Remove dead code

--- src/pages/api/hello.ts
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/basic-features/typescript#api-routes
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function hello (req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'John Doe' })
-}
