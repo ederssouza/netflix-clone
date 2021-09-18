b8ca1f12f398a8ca6940230d7b06fa1b1568faa5
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-18 17:00:34 -0300

    Toggle items length when on click link

--- src/components/Sidebar/SidebarBox/index.tsx
+++ src/components/Sidebar/SidebarBox/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 import styles from './styles.module.scss'
 
@@ -14,29 +15,61 @@ interface ISidebarBoxProps {
 }
 
 export function SidebarBox ({ title, mediaType, items }: ISidebarBoxProps) {
+  const [showMore, setShowMore] = useState(true)
+  const [list, setList] = useState([])
+  const limit = 5
+
+  function handleShowMore () {
+    setShowMore(!showMore)
+    const list = showMore ? items.slice(0, limit) : items
+    setList(list)
+  }
+
+  useEffect(() => {
+    handleShowMore()
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div className={styles.sidebarBox}>
       {title && <h4 className={styles.sidebarBoxTitle}>{title}</h4>}
 
       {mediaType
         ? (
-        <ul className={styles.sidebarBoxList}>
-          {items.map((item, index, arr) => {
-            return index + 1 < arr.length
-              ? <li key={item.id}> <Link href={`/genre/${mediaType}/${item.id}`}>{item.name}</Link>, </li>
-              : <li key={item.id}><Link href={`/genre/${mediaType}/${item.id}`}>{item.name}</Link></li>
-          })}
-        </ul>
+          <>
+            {list.map((item, index, arr) => {
+              const coma = index + 1 < arr.length ? ', ' : ''
+
+              return (
+                <span className={styles.sidebarBoxItem} key={item.id}>
+                  <Link href={`/genre/${mediaType}/${item.id}`}>{item.name}</Link>{coma}
+                </span>
+              )
+            })}
+          </>
           )
         : (
-        <ul className={styles.sidebarBoxList}>
-          {items.map((item, index, arr) => {
-            return index + 1 < arr.length
-              ? <li key={item.id}> {item.name}, </li>
-              : <li key={item.id}>{item.name}</li>
+          <>
+          {list.map((item, index, arr) => {
+            const coma = index + 1 < arr.length ? ', ' : ''
+
+            return (
+              <span className={styles.sidebarBoxItem} key={item.id}>
+                {item.name}{coma}
+              </span>
+            )
           })}
-        </ul>
+          </>
           )}
+
+      {items.length > limit && (
+        <a
+          className={styles.sidebarBoxListShowMore}
+          onClick={handleShowMore}
+          data-testid="sidebar-box-show-more"
+        >
+          {showMore ? '[ver menos -]' : '[ver mais +]'}
+        </a>
+      )}
     </div>
   )
 }
