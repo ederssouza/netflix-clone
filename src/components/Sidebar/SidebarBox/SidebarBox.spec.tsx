a7678c3b7c197eac610f2d1108f89b7135a0803b
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-18 16:59:13 -0300

    Add more tests

--- src/components/Sidebar/SidebarBox/SidebarBox.spec.tsx
+++ src/components/Sidebar/SidebarBox/SidebarBox.spec.tsx
@@ -1,14 +1,25 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import { SidebarBox } from '.'
 import { genresMock } from '../../../tests/mocks/tmdb'
 
 describe('SidebarBox component', () => {
   it('should render list item without link', () => {
-    render(<SidebarBox title="Adventure" items={genresMock} />)
+    render(<SidebarBox title="Adventure" items={genresMock.slice(0, 3)} />)
   })
 
-  it('should render list item with link', () => {
+  it('should show more items when click on "show more" link', () => {
     render(<SidebarBox title="Adventure" mediaType="drama" items={genresMock} />)
+
+    const $sidebarBoxShowMore = screen.getByTestId('sidebar-box-show-more')
+    fireEvent.click($sidebarBoxShowMore)
+  })
+
+  it('should show less items when click on "show less" link', () => {
+    render(<SidebarBox title="Adventure" mediaType="drama" items={genresMock} />)
+
+    const $sidebarBoxShowMore = screen.getByTestId('sidebar-box-show-more')
+    fireEvent.click($sidebarBoxShowMore)
+    fireEvent.click($sidebarBoxShowMore)
   })
 })
