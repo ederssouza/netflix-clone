d8401ef94c31f6aed8286e47e9f97fa11bbf1f7f
Eder Sampaio <edersampaio@outlook.com.br>
2021-09-18 12:53:26 -0300

    Add mediaType prop

--- src/components/Sidebar/SidebarBox/index.tsx
+++ src/components/Sidebar/SidebarBox/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import styles from './styles.module.scss'
 
 interface IItems {
@@ -7,21 +9,34 @@ interface IItems {
 
 interface ISidebarBoxProps {
   title?: string
+  mediaType?: string
   items: IItems[]
 }
 
-export function SidebarBox ({ title, items }: ISidebarBoxProps) {
+export function SidebarBox ({ title, mediaType, items }: ISidebarBoxProps) {
   return (
     <div className={styles.sidebarBox}>
       {title && <h4 className={styles.sidebarBoxTitle}>{title}</h4>}
 
-      <ul className={styles.sidebarBoxList}>
-        {items.map((item, index, arr) => {
-          return index + 1 < arr.length
-            ? <li key={item.id}> {item.name}, </li>
-            : <li key={item.id}>{item.name}</li>
-        })}
-      </ul>
+      {mediaType
+        ? (
+        <ul className={styles.sidebarBoxList}>
+          {items.map((item, index, arr) => {
+            return index + 1 < arr.length
+              ? <li key={item.id}> <Link href={`/genre/${mediaType}/${item.id}`}>{item.name}</Link>, </li>
+              : <li key={item.id}><Link href={`/genre/${mediaType}/${item.id}`}>{item.name}</Link></li>
+          })}
+        </ul>
+          )
+        : (
+        <ul className={styles.sidebarBoxList}>
+          {items.map((item, index, arr) => {
+            return index + 1 < arr.length
+              ? <li key={item.id}> {item.name}, </li>
+              : <li key={item.id}>{item.name}</li>
+          })}
+        </ul>
+          )}
     </div>
   )
 }
